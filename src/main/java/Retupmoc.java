8238103451e2c5e05b18e0ffcc7b4865f89730fc
Li Yi Cheng <liyicheng2003@gmail.com>
2026-01-13 13:27:17 +0800

    Add Rename, Greet, Exit feature

---
+++ src/main/java/Retupmoc.java
@@ -0,0 +1,22 @@
+public class Retupmoc {
+    public static void main(String[] args) {
+        printHorizontalLine();
+        printGreeting();
+        printHorizontalLine();
+        printGoodbye();
+        printHorizontalLine();
+    }
+
+    private static void printGreeting() {
+        System.out.println(" Hello! I'm Retupmoc");
+        System.out.println(" What can I do for you?");
+    }
+
+    private static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void printGoodbye() {
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+}
