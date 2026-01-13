02ef8b8a459797752de48fdc8b031b70230e0bf4
Li Yi Cheng <liyicheng2003@gmail.com>
2026-01-13 21:34:05 +0800

    Implement Task class, marking task as done

--- src/main/java/Retupmoc.java
+++ src/main/java/Retupmoc.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Retupmoc {
 
     private static final Scanner s = new Scanner(System.in);
-    private static final List<String> list = new ArrayList<>();
+    private static final List<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         printHorizontalLine();
@@ -38,9 +38,16 @@ public class Retupmoc {
     }
 
     private static void processUserInput(String input) {
-        switch (input.toLowerCase()) {
+        String[] command = input.trim().split("\\s+");
+        switch (command[0].toLowerCase()) {
             case "list":
                 displayList();
+                break;
+            case "mark":
+                markTaskDone(Integer.parseInt(command[1]) - 1);
+                break;
+            case "unmark":
+                markTaskNotDone(Integer.parseInt(command[1]) - 1);
                 break;
             case "bye":
                 printGoodbye();
@@ -51,15 +58,29 @@ public class Retupmoc {
         }
     }
 
-    private static void addToList(String task) {
-        list.add(task);
-        System.out.println("added: " + task);
+    private static void addToList(String description) {
+        list.add(new Task(description));
+        System.out.println("added: " + description);
     }
 
     private static void displayList() {
         for (int i = 0; i < list.size(); i++) {
             System.out.println((i + 1) + ". " + list.get(i));
         }
+    }
+
+    private static void markTaskDone(int taskNo) {
+        Task task = list.get(taskNo);
+        task.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task);
+    }
+
+    private static void markTaskNotDone(int taskNo) {
+        Task task = list.get(taskNo);
+        task.markAsNotDone();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(task);
     }
 
 }
